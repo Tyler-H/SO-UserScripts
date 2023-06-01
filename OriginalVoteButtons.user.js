e378b4260f41eceb8ed92804b1210fdec39a84d8
double beep <38133098+double-beep@users.noreply.github.com>
2023-06-01 13:43:34 +0000

    Use `GM_addStyle`

--- OriginalVoteButtons.user.js
+++ OriginalVoteButtons.user.js
@@ -11,41 +11,56 @@
 // @match        https://*.serverfault.com/*
 // @match        https://*.askubuntu.com/*
 // @match        https://*.mathoverflow.net/*
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function () {
-    const allbtns = document.querySelectorAll("button[class^='js-vote-']");
-    const downsvg = document.querySelectorAll('.js-vote-down-btn .iconArrowDown');
-    const upsvg = document.querySelectorAll('.js-vote-up-btn .iconArrowUp');
+    const updownsvg = document.querySelectorAll('.js-vote-down-btn .iconArrowDown, .js-vote-up-btn .iconArrowUp');
     const downpath = document.querySelectorAll('button.js-vote-down-btn svg.iconArrowDown path');
     const uppath = document.querySelectorAll('button.js-vote-up-btn svg.iconArrowUp path');
 
     // set attribute values for the SVG elements
-    [...downsvg, ...upsvg]
-        .forEach(svg => {
-            svg.setAttribute("height", "36");
-            svg.setAttribute("width", "36");
-            svg.setAttribute("viewBox", "0 0 36 36");
-        });
+    updownsvg.forEach(svg => {
+        svg.setAttribute("height", "36");
+        svg.setAttribute("width", "36");
+        svg.setAttribute("viewBox", "0 0 36 36");
+    });
 
     // set SVG path values to resize the SVGs themselves
     downpath.forEach(path => path.setAttribute('d', 'M2 11h32L18 27 2 11Z'));
     uppath.forEach(path => path.setAttribute('d', 'M2 25h32L18 9 2 25Z'));
 
-    //remove border & revert padding
-    allbtns.forEach(x => x.setAttribute('style', 'border: 1px solid transparent !important; padding: 1px 4px'));
+    GM_addStyle(`
+        button[class^='js-vote-'] {
+            /* remove border & revert padding */
+            border: 1px solid transparent !important;
+            padding: 1px 4px;
 
-    //revert hover styles
-    document.head.insertAdjacentHTML("beforeend", `<style>button[class^="js-vote-"].s-btn:hover { color: var(--blue-500) !important; background: none !important; }</style>`);
-    allbtns.forEach(x => x.style.setProperty('background-color', 'transparent', 'important'));
+            /* revert :hover styles */
+            background-color: transparent !important;
+        }
 
-    //revert :focus styles
-    document.head.insertAdjacentHTML("beforeend", `<style>button[class^="js-vote-"].s-btn:focus { box-shadow: none; }</style>`);
+        /* revert :hover styles */
+        button[class^="js-vote-"].s-btn:hover {
+            color: var(--blue-500) !important;
+            background: none !important;
+        }
 
-    //revert :active styles
-    document.head.insertAdjacentHTML("beforeend", `<style>button[class^="js-vote-"].s-btn:active { background: none }</style>`);
+        /* revert :focus styles */
+        button[class^="js-vote-"].s-btn:focus {
+            box-shadow: none;
+        }
 
-    //de-emphasize color brightness of buttons while leaving brightness of score
+        /* revert :active styles */
+        button[class^="js-vote-"].s-btn:active {
+            background: none
+        }
+
+        /* de-emphasize color brightness of buttons while leaving brightness of score */
+        button[class^="js-vote-"].fc-black-700 {
+            color: rgb(186, 191, 196) !important;
+        }
+    `);
+
     //allbtns.forEach ( x => x.style.setProperty('color','rgb(105, 111, 117)',''));
-    document.head.insertAdjacentHTML("beforeend", `<style>button[class^="js-vote-"].fc-black-700 { color: rgb(186, 191, 196) !important; }</style>`);
 })();
