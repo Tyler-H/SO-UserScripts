552834042fff2a38f4e05ff9104a926cbb0279ce
Tyler <206492+Tyler-H@users.noreply.github.com>
2025-01-07 13:11:47 -0500

    Update RevertVoteLabelTo0.js

--- RevertVoteLabelTo0.js
+++ RevertVoteLabelTo0.js
@@ -14,6 +14,8 @@
 // ==/UserScript==
 
 window.addEventListener("load", function(event){
+    //this is required because the label replacement A/B test appears to be implemented via a post-load JS script...
+    //instead of being done properly on the SO server side.
     setTimeout(updatePostLabels, 50);
 
     function updatePostLabels() {
@@ -22,6 +24,7 @@ window.addEventListener("load", function(event){
 
         //loop through each post score label in the NodeList. Where the innerHTML equals "Vote", replace it with "0" instead.
         allPostLabels.forEach ( x => x.innerHTML = x.innerHTML.replace('Vote','0'));
+        //loop through each post score label in the Nodelist and reset the font-size back to 21px. Why it was set smaller for the Vote label is unknown by anyone with sense.
         allPostLabels.forEach ( y => y.setAttribute('style', 'font-size: 21px !important'));
     };
 });
